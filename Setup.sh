17ddbf2a165bc4aab00e13da6e6a5be8c0af46cb
1337Xcode <64787166+1337Xcode@users.noreply.github.com>
2020-09-06 22:37:03 +0530

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,2 @@
+##//!@ERR0Rxx
+pkg update -y && pkg upgrade -y && pkg install python -y && pkg install python 2 -y && pkg install pv -y && pkg install screenfetch -y && pkg install curses-utils && pip install --pip upgrade -y && pip install lolcat -y && pip install requests -y
